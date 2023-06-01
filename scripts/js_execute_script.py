ed1f24c5ee91ceeb55c29d57850d3752645a3cef
Dev0011111000101010 <yrist.list@gmail.com>
2023-05-31 18:07:24 -0700

    add js_execute_script.py

---
+++ scripts/js_execute_script.py
@@ -0,0 +1,6 @@
+class JS_Execute_Script:
+    COLOR_RED = "arguments[0].style.setProperty('color', 'red', 'important');"
+    BORDER_3PX_SOLID_RED = "arguments[0].style.setProperty('border', '3px', 'red', 'solid', 'important');"
+    COLOR_RED_and_BORDER_3PX_SOLID_RED = "arguments[0].style.setProperty('color', 'red', 'important');arguments[0].style.setProperty('border', '3px', 'red', 'solid', 'important');"
+    DISPLAY_NONE = "arguments[0].style.display = 'none';"
+    DISPLAY_BLOCK = "arguments[0].style.display = 'block';"
